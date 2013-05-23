2e722604ef58d06ca6dbdd685b2d3b1ccf01ab04
jpcik <jean.paul.ik@gmail.com>
2013-05-23 17:19:03 +0200

    initial version demo Java r2rml

---
+++ build.sbt
@@ -0,0 +1,34 @@
+name := "morph-starter"
+
+organization := "es.upm.fi.oeg.morph"
+
+version := "0.0.1"
+
+scalaVersion := "2.10.1"
+
+crossPaths := false
+
+libraryDependencies ++= Seq(
+  "es.upm.fi.oeg.morph" % "morph-core" % "1.0.3",
+  "es.upm.fi.oeg.morph" % "morph-querygen" % "1.0.3",
+ "es.upm.fi.oeg.morph" % "morph-r2rml-tc" % "1.0.2" intransitive,
+  "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
+  "org.scalacheck" % "scalacheck_2.10" % "1.10.0" % "test",
+  "com.typesafe.akka" %% "akka-actor" % "2.1.2",
+  "com.typesafe.akka" %% "akka-remote" % "2.1.2",
+  "com.typesafe.akka" %% "akka-kernel" % "2.1.2"
+)
+
+resolvers ++= Seq(
+  "aldebaran-releases" at "http://aldebaran.dia.fi.upm.es/artifactory/sstreams-releases-local",
+  "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+)
+
+EclipseKeys.createSrc := EclipseCreateSrc.Default + EclipseCreateSrc.Resource
+
+//javacOptions ++= Seq("-source", "1.7", "-target", "1.6")
+
+
+
+
+
---
+++ src/main/java/es/upm/fi/oeg/morph/DemoQueryJava.java
@@ -0,0 +1,59 @@
+package es.upm.fi.oeg.morph;
+
+import java.util.Properties;
+
+import org.apache.jena.riot.RiotWriter;
+
+import com.hp.hpl.jena.query.Dataset;
+
+import es.upm.fi.oeg.morph.execute.RdfGenerator;
+import es.upm.fi.oeg.morph.r2rml.R2rmlReader;
+import es.upm.fi.oeg.morph.relational.JDBCRelationalModel;
+import es.upm.fi.oeg.morph.relational.RelationalModel;
+import es.upm.fi.oeg.morph.tc.DBManager;
+import es.upm.fi.oeg.siq.tools.ParameterUtils;
+
+/**
+ * Demo for generating RDF from a relational Database through JDBC
+ * @author jpc
+ *
+ */
+public class DemoQueryJava {
+  Properties props=ParameterUtils.load(this.getClass().getResourceAsStream("/conf/config.properties"));
+  RelationalModel relat;
+  DBManager db;
+  
+  DemoQueryJava(){
+	relat=new JDBCRelationalModel(props);
+	db=new DBManager(props.getProperty("jdbc.driver"),props.getProperty("jdbc.source.url"),
+		      props.getProperty("jdbc.source.user"),props.getProperty("jdbc.source.password"),false);
+  }
+  
+  /**
+   * Create the database using a script.
+   * This is not necessary if you already have your database. By default this test uses an in-memory HSLQDB instance.
+   */
+  private void createDB(){
+	String script = ParameterUtils.loadQuery("data/d005/create.sql");
+    db.clearDB();
+    db.createDB(script);
+  }
+  
+  /**
+   * Generate RDF using the provided R2rml mapping
+   * Produces an RDF Dataset
+   * @param mappingFile R2RML mapping file location
+   */
+  public void generate(String mappingFile){
+	createDB();  
+	R2rmlReader reader=new R2rmlReader(mappingFile);    
+	RdfGenerator g=new RdfGenerator(reader,relat);
+	Dataset ds=g.generate();
+    RiotWriter.writeNQuads(System.out,ds.asDatasetGraph());  
+  }
+  
+  public static void main(String[] args){
+	DemoQueryJava dqj = new DemoQueryJava();
+	dqj.generate("data/d005/r2rmla.ttl");		    	    
+  }
+}
